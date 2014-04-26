bfc3ce945c6d5fe6f09f5a32b7e0311e1b583186
andrewdarvell <andrew@darvell.ru>
2014-04-27 00:27:51 +0400

    pom with jar

--- src/main/java/serialtest/Main.java
+++ src/main/java/serialtest/Main.java
@@ -1,5 +1,6 @@
 package serialtest;
 
+import jssc.SerialPort;
 import jssc.SerialPortList;
 
 public class Main {
@@ -8,6 +9,27 @@ public class Main {
         String[] portNames = SerialPortList.getPortNames();
         for(int i = 0; i < portNames.length; i++){
             System.out.println(portNames[i]);
+        }
+
+
+
+        try{
+            SerialPort serialPort = new SerialPort("/dev/ttyACM0");
+            serialPort.openPort();
+            serialPort.setParams(SerialPort.BAUDRATE_9600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+
+            while (1==1){
+                String data = serialPort.readString();
+                //И снова отправляем запрос
+                //serialPort.writeString("Get data");
+                System.out.println(data);
+
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
